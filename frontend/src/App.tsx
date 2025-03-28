9f9991d6ea82149ac0f3b42fbbb9fd85b9809129
aminovitch-web <amineparis97x@gmail.com>
2025-03-28 18:33:10 +0100

    Added Layout component to manage global structure (header, footer) in the router, the layout wrap all pages with Outlet to display dynamic content based on the route, all routes are rendered within the Layout component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import SignupPage from '@/features/authentification/pages/Signup'
 import HomePage from '@/features/public/pages/Home'
 import ContactPage from '@/features/public/pages/Contact'
 import PricingPage from '@/features/public/pages/Pricing'
+import Layout from '@/layouts/Layout'
 
 export default function App() {
   return (
@@ -11,11 +12,13 @@ export default function App() {
       <div>
         <h1>compressify</h1>
         <Routes>
+          <Route path="/" element={<Layout />}>
           <Route path="/login" element={<LoginPage />} />
           <Route path="/signup" element={<SignupPage />} />
           <Route path="/" element={<HomePage />} />
           <Route path="/contact" element={<ContactPage />} />
           <Route path="/pricing" element={<PricingPage />} />
+          </Route>
         </Routes>
       </div>
     </Router>
